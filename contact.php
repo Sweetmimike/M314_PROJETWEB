a560aea3feeb3d018d353ed4382abfbba3417d32
ca703737 <aurelien.carpentier@etu.unice.fr>
2018-12-02 00:20:12 +0100

    création contact.php et avancement de l'architecture

---
+++ contact.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Contact</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+</head>
+<body>
+
+	<header>
+		
+	</header>
+
+	<section>
+		<div id="form-container">
+			<form action="contact.php" method="post" id="mailto">
+				<label for="email">Email : <input type="text" name="email"></label>
+				<label for="objet">Objet : <input type="text" name="objet"></label>
+				<label for="">Votre message : <textarea name="msg" id="msg" cols="50" rows="6"></textarea></label>
+				<input type="submit" name="envoyer" value="Envoyer">
+			</form>
+		</div>
+	</section>
+	
+	<footer>
+		
+	</footer>
+
+	<?php 
+
+	if(isset($_POST['envoyer'])) {
+		if(!empty($_POST['email']) && !empty($_POST['objet']) && !empty($_POST['msg'])) {
+			$message = $_POST['msg'];
+			$objet = $_POST['objet'];
+			$expediteur = $_POST['email'];
+			$destinataire = 'aurelien.carpentier@etu.unice.fr';
+			$headers = 'From: '.$expediteur.'\r\n';
+
+			mail($destinataire, $objet, $message, $headers);
+		}
+		
+	}
+
+	?>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,10 +2,35 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Index</title>
+	<title>Accueil</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<p>salut comment ca va ?</p>
+	<header>
+		<nav>
+			<ul id="navbar">
+				<li class="navitem"><a href="index.php">Accueil</a></li>
+				<li class="navitem"><a href="">Produits</a></li>
+				<li class="navitem"><a href="contact.php">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+	<hr>
+
+	<section>
+		<div>
+			<p>
+				Bienvenue sur le site de notre projet PHP. Dans le cadre de notre DUT informatique, il nous a été proposé de réaliser un site web de e-commerce.
+			</p>
+			<p>
+				Le but étant de réaliser ce site avec un espace membre public et un espace d'administration dont l'accès ne sera autorisé qu'aux admins.
+			</p>
+		</div>
+	</section>
+
+	<hr>
+	<footer>Projet PHP - Carpentier Aurélien - Allari Damien - G1 - IUT de Nice</footer>
 	
 </body>
 </html>
