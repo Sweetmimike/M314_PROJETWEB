7cf0fb068eaf5ca72460485e228f191e61bf6d30
ca703737 <aurelien.carpentier@etu.unice.fr>
2018-12-08 22:37:37 +0100

    modif

---
+++ connexion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <title>Connexion</title>
+</head>
+<body>
+   <?php include('header.php');?>
+   
+   
+   
+   
+   
+    <?php include('footer.php');?>
+   
+    
+</body>
+</html>
