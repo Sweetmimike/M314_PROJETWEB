7513ef078c029b715f07f1b21e17ac112f80b432
ca703737 <aurelien.carpentier@etu.unice.fr>
2018-12-10 12:21:51 +0100

    inscription.php

--- inscription.php
+++ inscription.php
@@ -1,13 +1,13 @@
 <?php
 
 try {
-    
+
     $bdd = new PDO('mysql:host=localhost;dbname=projet_php;charset=utf8', 'root', '');
-    
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(Exception $e) {
-    
+
     die('Erreur : ' . $e ->getMessage());
-    
+
 }
 
 
@@ -34,21 +34,48 @@ try {
                     <label for="email">Email : <input type="text" name="email"></label>
                     <label for="mdp">Mot de passe : <input type="password" name="mdp"></label>
                     <label for="c_mdp">Confirmer : <input type="password" name="c_mdp"></label>
+                    <label for="c_mdp">Rue : <input type="text" name="rue"></label>
+                    <label for="c_mdp">Ville : <input type="text" name="ville"></label>
+                    <label for="c_mdp">Pays : <input type="text" name="pays"></label>
                     <input type="submit" name="envoyer" value="Envoyer">
                 </form>
             </div>
         </section>
 
+        <?php   
+
+        echo $_POST['nom'];
+        echo $_POST['prenom'];
+        echo $_POST['email'];
+
+        if(isset($_POST['envoyer'])) {
+            if(!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['email']) && !empty($_POST['mdp']) && !empty($_POST['c_mdp'])
+               && !empty($_POST['rue']) && !empty($_POST['ville']) && !empty($_POST['pays'])) {
+                if($_POST['mdp'] == $_POST['c_mdp']) {
+                    $req = $bdd->prepare('insert into client (nom, prenom, email,rue, ville, pays, mdp) values(:nom, :prenom, :email, :rue, :ville, :pays, :mdp)');
+                    $req->execute(array(
+                        'nom' => $_POST['nom'],
+                        'prenom' => $_POST['prenom'],
+                        'email' => $_POST['email'],
+                        'rue' => $_POST['rue'],
+                        'ville' => $_POST['ville'],
+                        'pays' => $_POST['pays'],
+                        'mdp' => $_POST['mdp']));
+                    echo '<p>Inscription reussi !</p>';
+                } else {
+                    echo '<p>Les mots de passes doivent être identiques</p>';
+                }
+            } else {
+                echo '<p>Tous les champs sont obligatoires !</p>';
+            }
+        }
+
+        ?>
+
 
         <?php include('footer.php');?>
-        
-        <?php 
-        
-        if(!isset($_POST['envoyer'])) {
-            
-        }
-        
-        ?>
+
+
 
     </body>
 </html>
