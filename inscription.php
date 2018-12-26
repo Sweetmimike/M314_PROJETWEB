b643ccfb86f66054c4e4bcf93d779402dbd7889b
Sweetmimike <aurecarp@live.fr>
2018-12-26 13:19:53 +0100

    modif

---
+++ inscription.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>Inscription</title>
+    </head>
+    <body>
+    
+    <form action="#">
+        <label for="name"><input type="text" name="nom"></label>
+        <label for="name"><input type="text" name="nom"></label>
+        <label for="name"><input type="text" name="nom"></label>
+        <label for="name"><input type="text" name="nom"></label>
+    </form>
+
+    </body>
+</html>
